98923bff2eede74d088aab3c30f6dbf7fa524faf
Wen-Kai HSU <kylewhsu@gmail.com>
2022-11-12 14:21:09 +0800

    1112自訂函數

--- lesson6_1.py
+++ lesson6_1.py
@@ -1,7 +1,15 @@
-def sayhello():
-    print("Hello!")
+def sayhello(): #沒有參數的funtion
+    print("Hello!") 
+
+def sayhello_withname(name): #參數是一個變數，name僅可使用於程式區塊，別的地方無法使用，有一個參數的FUNtion
+    print(f"Hello﹑{name}")
+
+def square_area(side): #有一個參數 
+    area = side**2      
+    return area  # funtion內的變數
 
 
 if __name__ =="__main__":
-    sayhello()
-    #
+    side = eval(input("請輸入正方形的邊"))
+    area = square_area(side) #引數值傳遞到參數內   area 、side 為文件變數
+    print(f"正方形,一邊為{side},面積為{area}")
